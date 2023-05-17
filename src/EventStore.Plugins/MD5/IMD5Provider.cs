ebfe77c8e1a1cce880992e001d2e1b022944d896
shaan1337 <sniper111@gmail.com>
2023-05-17 09:34:20 +0400

    Add interface for MD5 plugins

---
+++ src/EventStore.Plugins/MD5/IMD5Provider.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+
+namespace EventStore.Plugins.MD5;
+
+public interface IMD5Provider {
+	/// <summary>
+	///     Creates an instance of the MD5 hash algorithm implementation
+	/// </summary>
+	HashAlgorithm Create();
+}
