7300723aafc19f75a730ca753577e950eeceb52c
YoEight <yo.eight@gmail.com>
2024-04-16 13:35:20 -0400

    Support dependency injection in plugins.

--- src/EventStore.Plugins/Subsystems/ISubsystem.cs
+++ src/EventStore.Plugins/Subsystems/ISubsystem.cs
@@ -7,10 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace EventStore.Plugins.Subsystems;
 
-public interface ISubsystem {
+public interface ISubsystem : IConfigureServices {
 	string Name { get; }
-	IApplicationBuilder Configure(IApplicationBuilder builder);
-	IServiceCollection ConfigureServices(IServiceCollection services, IConfiguration configuration);
 	void CollectTelemetry(Action<string, JsonNode> reply);
 	Task Start();
 	Task Stop();
