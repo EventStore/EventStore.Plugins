34c655e1c24405a2a2f4e89dde75cf3c17c46e6f
Timothy Coleman <timothy.coleman@gmail.com>
2024-04-26 13:40:57 +0100

    Add validation to License
    - The Licence has a public key built in (on balance it seems easier, when we want to change it, to change it here than storing it elsewhere)
    - It validates on construction that the jwt has been signed with the corresponding private key
    - It has a IsValidAsync method that plugins should call to check (in case someone changed it with reflection)
    - The private key is secret, but not that secret because it does have to shipped to the customer and it is quite visible in the assembly
      (but by the time they are looking in assemblies they can do that also with the protected plugins themselves)

---
+++ src/EventStore.Plugins.Tests/Licensing/LicenseTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using EventStore.Plugins.Licensing;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using Xunit;
+
+namespace EventStore.Plugins.Tests.Licensing;
+
+public class LicenseTests {
+	[Fact]
+	public async Task can_create_and_validate_license() {
+		// given
+		using var rsa = RSA.Create();
+		var privateKey = "MIIBOgIBAAJBALYnPoy4H96Uj2i/jA/H0lF5IHfTxrlUf9P+KapIdnNXtqJzlXsJ2GN5XbLN5EU8PIodLowci47o1rbFA9UqweECAwEAAQJAO/wkfxbLd/MYXvhlWXUGb8ohxRQ6pyGKjvduJSODzmuPPXmbCIxJZdw8XzGKpL9FGp0EHgIxPJuUl3J3xPNQGQIhAO+J7N2wCZGJvTmbhWwAlw6CvuVX4qCu7a/E0Rgvdyl7AiEAwqvEjxr7ISDizreJQrJ2U+SQhgrlcngD3j6FBcYSPVMCIQCK5O3sybNyqWyIZ85ghqMQbe2k7GXjiCsYXUZlALjW2wIgTxV4xKoxW0QA3/mvyWi2qV4xWEMU82vOJns/jxjoUAECIDm9wZGsyCDQBIx3h9eBODnQeI/b+iSe6/j8A9V7vyxU";
+
+		rsa.ImportRSAPrivateKey(Convert.FromBase64String(privateKey), out _);
+		var tokenHandler = new JsonWebTokenHandler();
+
+		var token = tokenHandler.CreateToken(new SecurityTokenDescriptor {
+			Audience = "esdb",
+			Issuer = "esdb",
+			Expires = DateTime.UtcNow + TimeSpan.FromHours(1),
+			Claims = new Dictionary<string, object>() {
+				{ "foo", "bar"},
+			},
+			SigningCredentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256),
+		});
+
+		// when
+		var license = await License.CreateAsync(token);
+
+		// then
+		// the constructor guarantees this, but we consider the case that someone changes
+		// the token using reflection.
+		// check repeatedly because of https://github.com/dotnet/runtime/issues/43087
+		Assert.True(await license.IsValidAsync());
+		Assert.True(await license.IsValidAsync());
+		Assert.True(await license.IsValidAsync());
+
+	}
+
+	[Fact]
+	public async Task fails_to_create_invalid_license() {
+		// given
+		using var rsa = RSA.Create();
+		var wrongPrivateKey = "MIIBOwIBAAJBALJPeVmYiRQ016TTg6/8mVBM1sO1tF3eYajn11MSLFUThg2GGFD1kvTMHQ+voWwOD5b4CBJaQOYPeS86PS5edS0CAwEAAQJBAJXi/Iz2DfSwXr0tF7ttyKqZjMbDDTUC5HEJQhWQZzezYtVR4qNc8g58994vqGft3aE9NKbrGIY11f96B/6xp/kCIQDrQEDDFOTvCIKnHkIt2w3uBqdSAdOI46EK53DKvCX6kwIhAMIJjpGl+uLTncEr9A1Ox8QIxPgILnd3ehM2fahr8uk/AiEAwKzDXgPC7TOfLpjNwxjic8znRXdRdZBZ2cBs1N78jBkCIAIsVTpwX3T25cdqFJupjDc32ezlOo//+JAKhjHCs7/FAiBUpSRSiAWQMMM/ib/RWck9/NIUCjRBRbwcVaUfPRtu3Q==";
+
+		rsa.ImportRSAPrivateKey(Convert.FromBase64String(wrongPrivateKey), out _);
+		var tokenHandler = new JsonWebTokenHandler();
+
+		var token = tokenHandler.CreateToken(new SecurityTokenDescriptor {
+			Audience = "esdb",
+			Issuer = "esdb",
+			Expires = DateTime.UtcNow + TimeSpan.FromHours(1),
+			Claims = new Dictionary<string, object>() {
+				{ "foo", "bar"},
+			},
+			SigningCredentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256),
+		});
+
+		// when/then
+		var ex = await Assert.ThrowsAsync<Exception>(async () => {
+			await License.CreateAsync(token);
+		});
+
+		Assert.Equal("Token could not be validated", ex.Message);
+	}
+
+	[Fact]
+	public void can_generate_key_pair() {
+		using var rsa = RSA.Create(512);
+		var privateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey());
+		var publicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+		Assert.NotEmpty(privateKey);
+		Assert.NotEmpty(publicKey);
+	}
+}
--- src/EventStore.Plugins/Licensing/License.cs
+++ src/EventStore.Plugins/Licensing/License.cs
@@ -1,5 +1,53 @@
-﻿using Microsoft.IdentityModel.JsonWebTokens;
+﻿using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EventStore.Plugins.Licensing;
 
-public record License(JsonWebToken Token);
+public sealed class License {
+	public JsonWebToken Token { get; }
+
+	private License(JsonWebToken token) {
+		Token = token;
+	}
+
+	public static async Task<License> CreateAsync(string jwt) {
+		var result = await ValidateTokenAsync(jwt);
+
+		if (!result.IsValid)
+			throw new Exception("Token could not be validated");
+
+		if (result.SecurityToken is not JsonWebToken token)
+			throw new Exception("Token is not a JWT");
+
+		return new License(token);
+	}
+
+	public async Task<bool> IsValidAsync() {
+		var result = await ValidateTokenAsync(Token.EncodedToken);
+		return result.IsValid;
+	}
+
+	private static async Task<TokenValidationResult> ValidateTokenAsync(string jwt) {
+		// not very satisfactory https://github.com/dotnet/runtime/issues/43087
+		CryptoProviderFactory.Default.CacheSignatureProviders = false;
+
+		var publicKey = "MEgCQQC2Jz6MuB/elI9ov4wPx9JReSB308a5VH/T/imqSHZzV7aic5V7CdhjeV2yzeRFPDyKHS6MHIuO6Na2xQPVKsHhAgMBAAE=";
+		using var rsa = RSA.Create();
+		rsa.ImportRSAPublicKey(Convert.FromBase64String(publicKey), out _);
+		var result = await new JsonWebTokenHandler().ValidateTokenAsync(
+			jwt,
+			new TokenValidationParameters {
+				ValidIssuer = "esdb",
+				ValidAudience = "esdb",
+				IssuerSigningKey = new RsaSecurityKey(rsa),
+				ValidateAudience = true,
+				ValidateIssuerSigningKey = true,
+				ValidateIssuer = true,
+				ValidateLifetime = true,
+			});
+		return result;
+	}
+}
